df436d0dd0bd15922a07ef5f9f71c0700b89539f
CaVOsO <cavoso@gmail.com>
2024-11-08 18:40:04 -0300

    respaldo cambios

---
+++ Extensiones/Double.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yui.Extensiones
+{
+    public static class DoubleExtension
+    {
+        public static String SeparadorMiles(this double aString, int dec = 0, String signo = "")
+        {
+            try
+            {
+                return Funciones.Comunes.SeparadorMiles(Convert.ToInt32(aString), dec, signo);
+            }
+            catch (Exception)
+            {
+                return aString.ToString();
+            }
+
+        }
+    }
+}
